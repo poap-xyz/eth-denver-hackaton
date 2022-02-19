6d808904042ae505352f35894086d6556ce9d85c
Agustin Vazquez <agustin@poap.io>
2022-02-19 12:25:28 -0700

    Typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 import { AccountsModule } from './accounts/accounts.module';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [EventsModule, AccountsModule, PostsModule],
+  imports: [
+    EventsModule,
+    AccountsModule,
+    PostsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'poap_ethdenver',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
