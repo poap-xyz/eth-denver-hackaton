6fea87e00149f81cdb22915d6575eca4836bc8ad
Nacho <nachonegrocaino@gmail.com>
2022-02-19 21:44:28 -0700

    add event service with axions

--- backend/src/events/events.service.ts
+++ backend/src/events/events.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PostsService } from '../posts/posts.service';
 import { Event } from './entities/event.entity';
+import axios from 'axios';
 
 @Injectable()
 export class EventsService {
   constructor(private readonly postsService: PostsService) {}
 
   private static async findEventFromPOAPApi(id: number): Promise<Event | null> {
-    const response = await fetch(`https://api.poap.xyz/events/id/${id}`);
-    if (response.status !== 200) {
-      return null;
-    }
-    return JSON.parse(await response.text());
+    const response = await axios(`https://api.poap.xyz/events/id/${id}`);
+    return await response.data;
   }
 
   async findOne(id: number): Promise<Event | null> {
