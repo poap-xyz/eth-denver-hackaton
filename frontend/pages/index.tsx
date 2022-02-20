a2a688c59dbf74f86de99e34ed02c929953b68f6
Toto <19580671+tomso11@users.noreply.github.com>
2022-02-19 18:23:39 -0700

    merge

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import { Navbar } from './components/Navbar'
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
     <>
+    <Navbar></Navbar>
       <h1 className={styles.title}>
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
