0484d436a9dff702f35f49e4780cdd74359dc246
rlajous <rodrigo@lajous.com.ar>
2022-02-20 00:34:37 -0700

    Fix scan

--- frontend/components/EventCard.tsx
+++ frontend/components/EventCard.tsx
@@ -1,14 +1,6 @@
 import { Event } from "../types/types";
-import { useState, useEffect, useContext, memo } from "react";
-import Image from "next/image";
-import classnames from "classnames";
 import dayjs from "dayjs";
 import Link from 'next/link'
-
-// import { LayoutContext } from 'context/layout'
-import Img from '../components/Img'
-// import { imageLoader, getImageUrl, wait } from 'lib'
-// import PoapSVG from 'component/Poap/SVG'
 
 import styles from "../styles/EventCard.module.scss";
 
@@ -24,33 +16,18 @@ const EventCard = ( { address, event }: EventCardProps) => {
 
   return (
     <Link href={`/address/${address}/${event.id}/feed`}>
-    <div className={styles.poap} >
-      <div className={styles.background}>
-        <Img
-          className={styles.animate}
-          src={`${event.image_url}`}
-        />
-      </div>
-
-      <div className={styles.content}>
-        <div className={styles.imageContainer}>
-          <div className={styles.imageAr}>
-            <div className={styles.image}>
-              <Img src={event.image_url} />
-            </div>
-          </div>
+      <div className={styles.event}>
+        <div className={styles.image}>
+          <img src={event.image_url} alt="" className={styles.image} />
         </div>
-
         <div className={styles.name}>
           <h3>{event.name}</h3>
         </div>
-
         <div className={styles.info}>
           {dayjs(event.start_date).format("MMM D, YYYY")}
           {event.city ? ` - ${event.city}` : ""}
         </div>
       </div>
-    </div>
     </Link>
   );
 };
