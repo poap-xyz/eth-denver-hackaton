1fce600c91f1b490c90fe7e31bb3ae00cb3bd133
Nacho <nachonegrocaino@gmail.com>
2022-02-19 21:18:01 -0700

    fix compilation errors

--- backend/src/events/events.controller.ts
+++ backend/src/events/events.controller.ts
@@ -1,15 +1,5 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { EventsService } from './events.service';
-import { CreateEventDto } from './dto/create-event.dto';
-import { UpdateEventDto } from './dto/update-event.dto';
 
 @Controller('events')
 export class EventsController {
