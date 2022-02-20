4a32267df5ba3ee7856c93393ef046b3b557015e
Nacho <nachonegrocaino@gmail.com>
2022-02-19 21:23:58 -0700

    add modules exports and imports on post and event

--- backend/src/events/events.module.ts
+++ backend/src/events/events.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
+import { PostsModule } from '../posts/posts.module';
 
 @Module({
+  imports: [PostsModule],
   controllers: [EventsController],
   providers: [EventsService],
 })
