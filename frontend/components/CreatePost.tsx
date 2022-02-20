2109f05dac1bcfa78cd77dbcd9b934f42f43371a
Toto <19580671+tomso11@users.noreply.github.com>
2022-02-20 02:41:20 -0700

    front end adjustments

--- frontend/components/CreatePost.tsx
+++ frontend/components/CreatePost.tsx
@@ -1,7 +1,7 @@
 import {createPost} from "../utils/api"
 import React, { useEffect, useState } from 'react'
 
-const CreatePost = ({events, address, eventId}:any) => {
+const CreatePost = ({events, address, event}:any) => {
     const [dataUri, setDataUri] = useState('');
     const [image, setImage] = useState<File | null>(null);
 
@@ -20,7 +20,7 @@ const CreatePost = ({events, address, eventId}:any) => {
             return;
         }
         const file = new Blob([image], {type: image.type})
-        const res = await createPost({address,description,file, eventId: +eventId});
+        const res = await createPost({address,description,file, event: +event});
         console.log(await res.data);
         
         //const result = await res.json()
